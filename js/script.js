9d9f2ccc96418f8a3d16d5aa81d19614a7b5ddbb
Kao <kao@appsynth.net>
2024-10-01 16:46:40 +0700

    Update the JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+function toggleMenu() {
+    var navbar = document.getElementById("navbar");
+    var navList = navbar.querySelector(".nav-list");
+    if (navList.className === "nav-list") {
+        navList.className += " open";
+    } else {
+        navList.className = "nav-list";
+    }
+}
 
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Website is loaded and ready.');
